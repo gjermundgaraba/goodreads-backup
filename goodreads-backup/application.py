af438570e32054c8d5c968ef05771cc983b178ff
Gjermund Bjaaes <bjaanes@gmail.com>
2016-09-22 16:20:01 +0200

    Added configuration and simple example of how to get goodreads data

--- goodreads-backup/application.py
+++ goodreads-backup/application.py
@@ -1,3 +1,14 @@
-import pymongo
+from urllib.request import urlopen
+from xml.dom.minidom import parse
+import configparser
 
-print("HELLO GOODREADS BACKUP STARTER PROJECT!")
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+user = config['CONFIG']['user']
+key = config['CONFIG']['key']
+
+test = urlopen("https://www.goodreads.com/review/list/" + user + ".xml?v=2&key=" + key)
+dom = parse(test)
+
+print(dom.toprettyxml())
