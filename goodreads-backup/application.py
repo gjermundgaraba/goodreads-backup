50543fb5c53a5c3164722dbaf8f476d6490fa681
Gjermund Bjaanes <bjaanes@gmail.com>
2016-09-25 13:34:10 +0200

    Get more data from xml, start nil checking

--- goodreads-backup/application.py
+++ goodreads-backup/application.py
@@ -16,4 +16,16 @@ reviews = dom.getElementsByTagName("review")
 for review in reviews:
     book = review.getElementsByTagName("book")[0]
     title = book.getElementsByTagName("title")[0]
+    id = book.getElementsByTagName("id")[0]
+    isbn = book.getElementsByTagName("isbn")[0]
+    isbn13 = book.getElementsByTagName("isbn13")[0]
+
     print(title.firstChild.data)
+    print(id.firstChild.data)
+    if isbn.getAttribute("nil") != "true":
+        print(isbn.firstChild.data)
+    if isbn.getAttribute("nil") != "true":
+        print(isbn13.firstChild.data)
+    print("")
+
+print("")
