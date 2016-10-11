847c552e2afce59606c2c2fc4d6e1afc6304f713
Gjermund Bjaanes <bjaanes@gmail.com>
2016-10-11 19:31:05 +0200

    Add books to data structure and save them to csv file

--- goodreads-backup/application.py
+++ goodreads-backup/application.py
@@ -1,6 +1,7 @@
 from urllib.request import urlopen
 from xml.dom.minidom import parse
 import configparser
+import csv
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -13,6 +14,8 @@ dom = parse(xml)
 
 reviews = dom.getElementsByTagName("review")
 
+books = []
+
 for review in reviews:
     book = review.getElementsByTagName("book")[0]
     title = book.getElementsByTagName("title")[0]
@@ -20,12 +23,20 @@ for review in reviews:
     isbn = book.getElementsByTagName("isbn")[0]
     isbn13 = book.getElementsByTagName("isbn13")[0]
 
-    print(title.firstChild.data)
-    print(id.firstChild.data)
-    if isbn.getAttribute("nil") != "true":
-        print(isbn.firstChild.data)
-    if isbn.getAttribute("nil") != "true":
-        print(isbn13.firstChild.data)
-    print("")
+    book = {
+        'title': title.firstChild.data,
+        'id': id.firstChild.data
+    }
 
-print("")
+    if isbn.getAttribute("nil") != "true":
+        book['isbn'] = isbn.firstChild.data
+    if isbn.getAttribute("nil") != "true":
+        book['isbn13'] = isbn13.firstChild.data
+
+    books.append(book)
+
+with open('books.csv', 'w', newline='') as csvfile:
+    spamwriter = csv.writer(csvfile, delimiter=',')
+
+    for book in books:
+        spamwriter.writerow([book['title'], book['id'], book['isbn'], book['isbn13']])
